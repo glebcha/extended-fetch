b82c3566c03385cda48c20300557c98ad4a922d5
Gleb Popov <glebcha@gmail.com>
2023-11-28 19:33:58 +0700

    feat: added auth middleware mocks

--- src/mock.ts
+++ src/mock.ts
@@ -1,4 +1,11 @@
+import { AuthMiddlewareParams } from './middleware/auth/types';
+
 export const query =  { id: 1, name: 'test' };
 
 export const endpoint = 'https://test.test/';
+export const authRefreshEndpoint = `${endpoint}auth/refresh`;
 export const queryInvalid = 'ERROR';
+
+export function getTokens(tokens?: unknown) {
+  return tokens as ReturnType<AuthMiddlewareParams['getTokens']> ?? { accessToken: '1234', refreshToken: 'refresh1234' };
+}
