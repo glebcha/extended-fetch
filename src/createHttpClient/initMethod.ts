fa9ee2aa43fe6e214c666de58b882fbd1328d83e
Gleb Popov <glebcha@gmail.com>
2024-03-11 22:36:29 +0700

    docs: initMethod

--- src/createHttpClient/initMethod.ts
+++ src/createHttpClient/initMethod.ts
@@ -9,6 +9,23 @@ import { is } from '../utils';
 
 import { createMethod } from './createMethod';
 
+/**
+ * @namespace httpClient
+ * @module initMethod
+ * @description Wrapper that return REST method.
+ * @param {string} method
+ * @param {Object} options
+ * @param {string} options.query
+ * @param {number} options.timeout
+ * @param {string} options.url
+ * @param {Function[]} options.middleware
+ * @param {Object} options.params
+ * @param {string} options.method
+ * @param {string} options.format
+ * @throws {Response & { formattedResponse: string | object }}
+ * @returns {Response} resolved or rejected promise
+ */
+
 export function initMethod(method: `${Lowercase<Methods>}`, options?: CreateHttpClientOptions) {
   return (methodOptions?: unknown) => {
 
